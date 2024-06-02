f5020c8608df7348afa0fea562f167bb82afe89a
Jovian Shilton Dsouza <dsouzajovian123@gmail.com>
2024-06-02 05:03:19 +0000

    add config files

---
+++ eslint.config.mjs
@@ -0,0 +1,22 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
+import { fixupConfigRules } from "@eslint/compat";
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+
+export default [
+  {
+  files: ['**/*.{js,mjs,cjs,jsx,mjsx,ts,tsx,mtsx}'],
+  languageOptions: { globals: globals.browser },
+  settings: {
+    react: {
+     version: "detect",
+    },
+  },
+  },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...fixupConfigRules(pluginReactConfig),
+  eslintPluginPrettierRecommended,
+];
