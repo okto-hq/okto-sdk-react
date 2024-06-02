d1eae4ec62fa1fe76389540146d3644cb0d892a4
Jovian Shilton Dsouza <dsouzajovian123@gmail.com>
2024-06-02 07:51:47 +0000

    ci: add ereact example

---
+++ example/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import 'okto-sdk-react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Okto SDK for React Example
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
